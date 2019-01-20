6e0af7a7f7e2733102041b1057e226d93859c04b
Lukas Striebel <lukas.striebel@hotmail.com>
2019-01-20 11:16:24 +0100

    Add push notification

--- app/src/main/java/com/example/lukas/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lukas/myapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.lukas.myapplication;
 
-import android.app.Application;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import 	android.os.Vibrator;
@@ -12,7 +16,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.support.v4.app.NotificationManagerCompat;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -21,10 +25,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    String org;
+    String preferenceOrigin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +38,20 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        org = PreferenceManager.getDefaultSharedPreferences(this).getString("origin", org);
+        final Context context = this.getBaseContext();
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        Date futureDate = new Date(new Date().getTime() + 86400000);
+        futureDate.setHours(11);
+        futureDate.setMinutes(15);
+        futureDate.setSeconds(0);
+        Intent intent = new Intent(context, MyAppReciever.class);
+
+        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, futureDate.getTime(), sender);
+
+
+        preferenceOrigin = PreferenceManager.getDefaultSharedPreferences(this).getString("origin", preferenceOrigin);
     }
 
     private final View.OnTouchListener mCheckNowListener = new View.OnTouchListener() {
@@ -59,46 +77,77 @@ public class MainActivity extends AppCompatActivity {
 
             EditText source = (EditText) findViewById(R.id.source);
             EditText destination = (EditText) findViewById(R.id.destination);
-            String src = "Buelach";//source.getText();
-            String dest = "Zuerich";//destination.getText();
-            String time = "07:35";
-            String getUrl = "http://transport.opendata.ch/v1/connections?from=" +
-                    src + "&to=" + dest + "&time=" + time +"&limit=1";
-
-            try {
-                URL url = new URL(getUrl);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-                String line;
-                StringBuilder stringBuilder = new StringBuilder();
-                
-                while ((line = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(line);                        
-                }
-                System.out.println(stringBuilder.toString());
-                final JSONObject response = new JSONObject(stringBuilder.toString());
-                final JSONArray connections = response.getJSONArray("connections");
-                JSONObject from = connections.getJSONObject(0).getJSONObject("from");
-                String delayString = from.getString("delay");
-                int delay = delayString == "null" ? 0 : Integer.parseInt(delayString);
-                if (delay == 0) {
-                    myAwesomeTextView.setText("No Delay known");
-                    myAwesomeTextView.setBackgroundColor(Color.GREEN);
-                    Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                    vibrator.vibrate(300);
-                } else {
-                    myAwesomeTextView.setText("Expected delay: " + delay + " minutes");
-                    myAwesomeTextView.setBackgroundColor(Color.RED);
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                myAwesomeTextView.setText("Connection Error. Try again later");
+            int delay = getDelay();
+            if (delay == 0) {
+                myAwesomeTextView.setText("No Delay known");
+                myAwesomeTextView.setBackgroundColor(Color.GREEN);
+                //Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                //vibrator.vibrate(300);
+                notifyUser("No Delay", "Train on Time");
+            } else {
+                myAwesomeTextView.setText("Expected delay: " + delay + " minutes");
+                myAwesomeTextView.setBackgroundColor(Color.RED);
+                notifyUser("Expected Delay", delay + " minutes");
             }
+
             return false;
         }
     };
+
+    private void notifyUser(String title, String text) {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_info_black_24dp)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1, mBuilder.build());
+    }
+
+    private class MyAppReciever extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            System.out.println("received");
+            startActivity(new Intent(context,  MainActivity.class));
+            int delay = getDelay();
+            notifyUser("Delay", delay + " minutes");
+        }
+    }
+
+    private int getDelay() {
+        String src = "Buelach";//source.getText();
+        String dest = "Zuerich";//destination.getText();
+        String time = "07:35";
+        String getUrl = "http://transport.opendata.ch/v1/connections?from=" +
+                src + "&to=" + dest + "&time=" + time +"&limit=1";
+
+        try {
+            URL url = new URL(getUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            System.out.println(stringBuilder.toString());
+            final JSONObject response = new JSONObject(stringBuilder.toString());
+            final JSONArray connections = response.getJSONArray("connections");
+            JSONObject from = connections.getJSONObject(0).getJSONObject("from");
+            String delayString = from.getString("delay");
+            return delayString == "null" ? 0 : Integer.parseInt(delayString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            //myAwesomeTextView.setText("Connection Error. Try again later");
+        }
+        return 0;
+    }
 }
